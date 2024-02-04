450f677666dc4deffec8f10602a5af3c888019ed
frenibo <frederic.borowiak@gmail.com>
2024-02-04 05:46:14 +0100

    calling functions from other files: wip

---
+++ preload.js
@@ -0,0 +1,7 @@
+var preload2 = function preload2() {
+    this.load.image('bug1', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_1.png');
+	this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png');
+	this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
+	this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
+	this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
+};
